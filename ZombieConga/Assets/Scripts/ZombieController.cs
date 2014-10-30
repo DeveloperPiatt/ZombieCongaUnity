a31dcd05bef501e214a9eae61108eff5cbfe4c36
DeveloperPiatt <developer.piatt@gmail.com>
2014-10-30 15:16:31 -0700

    zombie won't leave scene

--- ZombieConga/Assets/Scripts/ZombieController.cs
+++ ZombieConga/Assets/Scripts/ZombieController.cs
@@ -19,6 +19,31 @@ public class ZombieController : MonoBehaviour {
 		colliders [currentColliderIndex].enabled = true;
 	}
 
+	private void EnforceBounds()
+	{
+		Vector3 newPosition = transform.position;
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+		
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize;
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+		
+		if (newPosition.x < xMin || newPosition.x > xMax) {
+			newPosition.x = Mathf.Clamp (newPosition.x, xMin, xMax);
+			moveDirection.x = -moveDirection.x;
+		}
+
+		float yMax = mainCamera.orthographicSize;
+
+		if (newPosition.y < -yMax || newPosition.y > yMax) {
+			newPosition.y = Mathf.Clamp ( newPosition.y, -yMax, yMax);
+			moveDirection.y = -moveDirection.y;
+		}
+		
+		transform.position = newPosition;
+	}
+
 	void OnTriggerEnter2D (Collider2D other) 
 	{
 		Debug.Log ("Hit " + other.gameObject);
@@ -46,5 +71,8 @@ public class ZombieController : MonoBehaviour {
 		float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler (0,0,targetAngle), turnSpeed * Time.deltaTime);
 
+		EnforceBounds ();
 	}
+
+
 }
